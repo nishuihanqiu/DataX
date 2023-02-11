1c37647b66525a460f944c2550c0e03e93827168
liliangshan <liliangshanlls@gmail.com>
2023-02-11 20:01:46 +0800

    add hdfsreaderv1 and hdfswriterv1

---
+++ hdfsreaderv1/src/main/java/com/alibaba/datax/plugin/reader/hdfsreaderv1/Constant.java
@@ -0,0 +1,16 @@
+package com.alibaba.datax.plugin.reader.hdfsreaderv1;
+
+/**
+ * liliangshan.
+ */
+public class Constant {
+    public static final String SOURCE_FILES = "sourceFiles";
+    public static final String TEXT = "TEXT";
+    public static final String ORC = "ORC";
+    public static final String CSV = "CSV";
+    public static final String SEQ = "SEQ";
+    public static final String RC = "RC";
+    public static final String PAR = "PAR";
+
+
+}
---
+++ hdfsreaderv1/src/main/java/com/alibaba/datax/plugin/reader/hdfsreaderv1/HdfsFileType.java
@@ -0,0 +1,9 @@
+package com.alibaba.datax.plugin.reader.hdfsreaderv1;
+
+/**
+ * Created by mingya.wmy on 2015/8/22.
+ *
+ */
+public enum HdfsFileType {
+    ORC, SEQ, RC, CSV, TEXT, PAR,
+}
